cf1b932aae725a2f283cdfe4f696083e78137741
Nick Sullivan <nicholassull@gmail.com>
2021-12-12 16:25:16 -0800

    added business and UI logic and export/import for Journal
    Co-authored-by: Cesar Lopez <Lopez.cesar.aug@gmail.com>
    Co-authored-by: Ebru Rice <ebruerenb@gmail.com>

--- src/journal.js
+++ src/journal.js
@@ -2,7 +2,6 @@ export default function Journal(textArray, title) {
   this.textArray = textArray;
   this.title = title;
 }
-
 Journal.prototype.numberWords = function(){
   return this.textArray.length + 1;
 }
