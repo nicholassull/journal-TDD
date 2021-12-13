698f6319e84b4a275da9e1e4cd69b3e14602b8aa
Nick Sullivan <nicholassull@gmail.com>
2021-12-12 16:55:18 -0800

    add bootstrap and jquery
    Co-authored-by: Cesar Lopez <Lopez.cesar.aug@gmail.com>
    Co-authored-by: Ebru Rice <ebruerenb@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import Journal from './journal.js';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 $(document).ready(function() {
