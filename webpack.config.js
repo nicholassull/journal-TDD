67521c72f17034b450f7e5e812167c2bd3033ad1
Nick Sullivan <nicholassull@gmail.com>
2021-12-12 16:52:02 -0800

    add eslint
    Co-authored-by: Cesar Lopez <Lopez.cesar.aug@gmail.com>
    Co-authored-by: Ebru Rice <ebruerenb@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,10 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
@@ -24,6 +28,11 @@ module.exports = {
           'style-loader',
           'css-loader'
         ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
       }
     ]
   }
