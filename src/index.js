077565c35eefef0c70e5632cdd49e9bddf553337
Nick Sullivan <nicholassull@gmail.com>
2021-12-12 17:06:52 -0800

    update to a working dev environment
    Co-authored-by: Cesar Lopez <Lopez.cesar.aug@gmail.com>
    Co-authored-by: Ebru Rice <ebruerenb@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import Journal from './journal.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Journal from './journal.js';
 
 $(document).ready(function() {
   $('#text-form').submit(function(event) {
